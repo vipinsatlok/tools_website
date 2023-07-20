060cce27f0e832a9c6d0e7addc2294011a15b1f9
Vipin Das <118341086+vipinsatlok@users.noreply.github.com>
2023-07-20 15:47:53 +0000

    Init Project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import YoutubeThumbnailDownload from "@/tools/youtube/ThumbnailDownload";
+
+export default function Home() {
+  return (
+    <div>Home Page
+
+      <YoutubeThumbnailDownload />
+    </div>
+  )
+}
